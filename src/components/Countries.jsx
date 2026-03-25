020333e40cd0822e1fcc9ff0eb7dc538a0e35092
fh1683398 <fh1683398@gmail.com>
2026-03-25 23:56:11 +0600

    toggle visited list complete

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,14 +1,40 @@
 import Country from './Country';
-import { use } from 'react'
+import { use, useState } from 'react'
 
 const Countries = ({ countryPromise }) => {
+
+    const [visitedCountry, setVisitedCountry] = useState([])
+
+    const showVisitedCountry = (country) => {
+        const isVisited = visitedCountry.includes(country)
+
+        if (isVisited) {
+            const newList = visitedCountry.filter(item => item !== country)
+            setVisitedCountry(newList)
+        } else {
+            const newList = [...visitedCountry, country]
+            setVisitedCountry(newList)
+        }
+
+    }
+
     const countries = use(countryPromise)
 
-
     return (
-        <div>
-            {countries.map(country => <Country key={country.name.common} country={country}></Country>)}
-        </div>
+        <>
+            <h1>All countries</h1>
+            <p>Visited Countries : {visitedCountry.length}</p>
+            {
+                visitedCountry.map(country => <li>{country.name.common}</li>)
+            }
+            <div className='grid-display'>
+
+                {countries.map(country => <Country
+                    key={country.name.common}
+                    country={country}
+                    showVisitedCountry={showVisitedCountry}></Country>)}
+            </div>
+        </>
     );
 };
 
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 
-const Country = ({ country }) => {
+const Country = ({ country, showVisitedCountry }) => {
+
+    const [visited, setVisited] =useState(false)
+
+    const handleVisited = ()=>{
+        setVisited(!visited)
+        showVisitedCountry(country);
+    }
 
     return (
-        <div>
-            <p>Name : {country.name.common}</p>
+        <div className={`countryStyle ${visited && 'visitedStyle'}`}>
+            <h4>Name : {country.name.common}</h4>
+            <p>Official: {country.name.official}</p>
+            <button onClick={handleVisited}>{visited? "Visited": "Not visited"}</button>
         </div>
     );
 };
